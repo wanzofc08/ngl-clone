8146d97f039438a5048c8f60626125487ad8c5ea
wanzofc08 <wanzofc08@gmail.com>
2025-03-10 14:40:28 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,23 +5,17 @@ const confessionRoutes = require("./routes/confessionRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 connectDB();
-
 app.get("/", (req, res) => {
-    res.send("Server is up and running!");
+    res.send("lagi running bro!");
 });
 app.use("/", confessionRoutes);
 app.use("/", messageRoutes);
 app.use("/", userRoutes);
-
 app.use(errorHandler);
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
